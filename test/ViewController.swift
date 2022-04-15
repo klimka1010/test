514c5f791d71308e35f267002bef15b4def9e72c
klimka1010 <klimka1010@gmail.com>
2022-04-15 21:22:32 +0400

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by klimka1010 on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
